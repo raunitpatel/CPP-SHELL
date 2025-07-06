f1cc3a455f640fab9b3150b1c1a6c5c4ef3fedc7
root <root@raunitpatel.localdomain>
2025-07-06 16:50:17 +0000

    added echo commands

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,19 @@ int main() {
     if(input == "exit 0"){
       break;
     }
+    else if(input.find("echo") != string::npos){
+      string temp="";
+      bool flag=false;
+      for(auto x:input){
+        if(x==' ' and !flag){
+          flag=true;
+        }
+        else if(flag){
+          temp+=x;
+        }
+      }
+      cout<<temp<<endl;
+    }
     else{
       cout<<input<<": command not found"<<endl;
     }
