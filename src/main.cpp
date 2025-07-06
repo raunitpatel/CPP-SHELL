207a18beceecc9087cdd1bf068e228a13eb26020
root <root@raunitpatel.localdomain>
2025-07-06 16:09:48 +0000

    added REPL

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,16 @@ using namespace std;
 
 int main() {
   // Flush after every std::cout / std:cerr
-  cout << unitbuf;
-  cerr << unitbuf;
+  while(1){
+    cout << unitbuf;
+    cerr << unitbuf;
 
-  // Uncomment this block to pass the first stage
-  cout<<"$ ";
+    // Uncomment this block to pass the first stage
+    cout<<"$ ";
 
-  string input;
-  getline(std::cin, input);
-  cout<<input<<": command not found"<<endl;
+    string input;
+    getline(std::cin, input);
+    cout<<input<<": command not found"<<endl;
+  }
+  
 }
